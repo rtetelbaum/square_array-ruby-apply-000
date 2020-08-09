0d2c28e38a9654096ca756f05fa156420e040e06
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-09 03:40:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,9 +3,9 @@ numbers = [1, 2, 3]
 def square_array(numbers)
   numbers.each do |number|
     new_number = number ** 2
-    puts "#{new_number}"
     numbers << 
   end
 end
 
 square_array(numbers)
+puts "#{new_number}"
