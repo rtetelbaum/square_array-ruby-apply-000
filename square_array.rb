2d024a24948d44d083a9d421c0c49450b21e923b
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-09 03:34:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,12 +4,14 @@ def square_array(numbers)
   numbers.each do |number|
     new_number = number ** 2
     puts "#{new_number}"
-    def create_new_numbers_array
-      new_numbers[]
-    end
-    create_new_numbers_array
     new_numbers << new_number
   end
 end
+
+def create_new_numbers_array
+  new_numbers[]
+end
+
+create_new_numbers_array
 
 square_array(numbers)
