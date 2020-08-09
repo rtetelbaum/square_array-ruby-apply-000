c54d2640716d3cc4f63a1886958425475540d655
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-09 03:09:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,8 @@ numbers = [1, 2, 3]
 
 def square_array(numbers)
   numbers.each do |number|
-    number ** 2
-    new_numbers << 
+    new_number = number ** 2
+    new_numbers << new_number
   end
 end
 
